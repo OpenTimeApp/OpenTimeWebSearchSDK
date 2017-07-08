8bf772ac7dd83243b0f4b7b117b77c3d42b3467e
sergeome <sergeome@gmail.com>
2017-07-07 22:11:52 -0500

    <no-ticket> Update (SDK): Add places id field

--- src/sdk/apis/business/appointment/book-a-business-for-me-request/otws-book-a-business-for-me-request-request.ts
+++ src/sdk/apis/business/appointment/book-a-business-for-me-request/otws-book-a-business-for-me-request-request.ts
@@ -1,23 +1,30 @@
-import {OTWSAvailableTime} from "../../../otws-available-time";
 import {OTWSBookItForMeRequestRequest} from "../book-it-for-me-request/otws-book-it-for-me-request-request";
 export class OTWSBookABusinessForMeRequestRequest extends OTWSBookItForMeRequestRequest implements OTWSAPIRequest {
 
 
     private _businessID : number;
-
-    constructor(firstName: string, lastName: string, email: string, availableTimes: OTWSAvailableTime[], businessID: number) {
-        super(firstName, lastName, email, availableTimes);
-
-        this._businessID = businessID;
-    }
+    private _placesID: string;
 
     private _getBusinessID(){
         return this._businessID;
     }
 
+    private _getPlacesID(){
+        return this._placesID;
+    }
+
+    setPlacesID(placesID: string){
+        this._placesID = placesID;
+    }
+
+    setBusinessID(businessID: number){
+        this._businessID = businessID;
+    }
+
     getData(): any {
         let data = super.getData();
         data.business_id = this._getBusinessID();
+        data.places_id = this._getPlacesID();
         return data;
     }
 }
